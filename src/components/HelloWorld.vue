c32c39dd560fa4287c58902c6082563635fbede2
RickyHuang80 <hhuang@secor-am.com>
2021-01-21 18:51:06 -0500

    modify

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="hello">
+    <strong> I made it! </strong>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
